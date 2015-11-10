9d7dc77e6d1d3aef0b110e597d287380360a01d7
Baldeep <baldeepsingh.93@gmail.com>
2015-11-10 21:45:59 +0000

    Added Next State class and begun work on parser \nThe parset might not be neat, but it does the job for now

--- UnversalTM/src/HashTest.java
+++ UnversalTM/src/HashTest.java
@@ -4,6 +4,39 @@ import java.util.Map;
 public class HashTest {
 
 	public static void main(String[] args) {
+		State s1 = new State("q1", 'c');
+		State s2 = new State("q2", '2');
+		State s3 = new State("q1", '1');
+		State s4 = new State("q1", '2');
+		State s5 = new State("q1", '2');
+		
+		NextState ns1 = new NextState("q1", 'c', Direction.R);
+		NextState ns2 = new NextState("q2", ' ', Direction.L);
+		NextState ns3 = new NextState("q1", '2', Direction.R);
+		NextState ns4 = new NextState("q3", '1', Direction.L);
+		NextState ns5 = new NextState("q1", 'c', Direction.R);
+		
+		
+		Map<State, NextState> map = new HashMap<>();
+		map.put(s1, ns1);
+		map.put(s2, ns2);
+		map.put(s4, ns1);
+		
+		System.out.println("s1 hash: " + s1.hashCode());
+		System.out.println("ns1 hash: " + ns1.hashCode());
+		System.out.println("ns3 hash: " + ns3.hashCode());
+		System.out.println("ns5 hash: " + ns5.hashCode());
+		System.out.println("s1 = ns1: " + s1.equals(ns1));
+		System.out.println("ns1 = ns5: " + ns1.equals(ns5));
+		
+		NextState t1 = map.get(s1);
+		System.out.println("check found: " + ns1.equals(t1));
+		System.out.println("t1 = (" + t1.getState() + ", " 
+									+ t1.getOutput() + ", " 
+									+ t1.getDirection() + ")");
+	}
+	
+	public static void testKeys(){
 		State s1 = new State("q1", '1');
 		State s2 = new State("q2", '2');
 		State s3 = new State("q1", '1');
